f030a0f76d8ffd6e3aa8e8018a9a9eb2329e35cb
Noura <culurienneldoreth@gmail.com>
2018-08-29 00:21:55 -0700

    just write the mic val to serial. do all the processing in raspberry pi

--- Arduino/mic_bpm/mic_bpm.ino
+++ Arduino/mic_bpm/mic_bpm.ino
@@ -1,32 +1,9 @@
 #define micPin 0
-#define sampling_period_ms 20
+#define sampling_period_ms 10
 
-// LOW PASS FILTER
-
-class Filter
-{
-    public:
-        Filter()
-        {
-            curr = 0.0;
-            prev = 0.0;
-        }
-    private:
-        float curr;
-        float prev;
-    public:
-        float step(float x)
-        {
-            prev = curr;
-            curr = 0.99 * prev + 0.01 * x;
-            return curr;
-        }
-};
-
-
-Filter myFilter;
 int micVal = 0;
-float micValFiltered = 0;
+int prevT = 0;
+int T = 0;
 
 void setup() {
   pinMode(micPin, INPUT);
@@ -34,11 +11,9 @@ void setup() {
 }
 
 void loop() {
-  
-  micVal = abs(analogRead(micPin)-512);
-  micValFiltered = myFilter.step(micVal);
+  micVal = analogRead(micPin);
 
-  Serial.println(micValFiltered);
+  Serial.println(micVal);
 
   delay(sampling_period_ms);
 }
