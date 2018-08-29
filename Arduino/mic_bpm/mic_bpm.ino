0d4dcbc0e40364c7c1ebfda929c7e6c764c1c3c3
Noura <culurienneldoreth@gmail.com>
2018-08-29 00:08:58 -0700

    just doing a low pass filter

--- Arduino/mic_bpm/mic_bpm.ino
+++ Arduino/mic_bpm/mic_bpm.ino
@@ -1,6 +1,5 @@
-#include "arduinoFFT.h"
-
 #define micPin 0
+#define sampling_period_ms 20
 
 // LOW PASS FILTER
 
@@ -19,78 +18,15 @@ class Filter
         float step(float x)
         {
             prev = curr;
-            curr = 0.95 * prev + 0.05 * x;
+            curr = 0.99 * prev + 0.01 * x;
             return curr;
         }
 };
-
-// BUFFER THAT UPDATES ITSELF AND WILL GIVE YOU A ROLLING AVERAGE
-
-class BufferAverage {
-  public:
-    BufferAverage(int N);
-    ~BufferAverage();
-    void updateB(float val);
-    float averageB();
-    void printB();
-
-  private:
-    int _NSAMPLES = 0;
-    float* _buf = 0;
-    int _bufIndex = 0;
-    float _bufVal = 0;
-    Filter myFilter;
-};
-
-BufferAverage::BufferAverage(int N) {
-  _NSAMPLES = N;
-  
-  if (_buf != 0) {
-    delete [] _buf;
-  }
-  _buf = new float[_NSAMPLES*2];
-
-  for (int i = 0; i < _NSAMPLES*2; i++) {
-    _buf[i] = 0.0;
-  }
-}
-
-BufferAverage::~BufferAverage() {
-  if (_buf != 0) {
-    delete [] _buf;
-  }
-}
-
-void BufferAverage::updateB(float val) {
-  _buf[_bufIndex] = val;
-  _buf[_bufIndex + _NSAMPLES] = val;
-  _bufIndex += 1;
-  if (_bufIndex > _NSAMPLES-1) {
-    _bufIndex = 0;
-  }
-}
-
-float BufferAverage::averageB() {
-  float avg = 0;
-  for (int i = _bufIndex + _NSAMPLES; i--; i > _bufIndex) {
-    avg += _buf[i] / float(_NSAMPLES);
-  }
-  return avg;
-}
-
-void BufferAverage::printB() {
-  Serial.println("\n\n BufferAverage::printB()");
-  for (int i = 0; i < _NSAMPLES*2; i++) {
-    if (i == _NSAMPLES) Serial.println();
-    Serial.print(_buf[i]); Serial.print(", ");
-  }
-}
 
 
 Filter myFilter;
 int micVal = 0;
 float micValFiltered = 0;
-float micValFilteredPrev = 0;
 
 void setup() {
   pinMode(micPin, INPUT);
@@ -98,11 +34,12 @@ void setup() {
 }
 
 void loop() {
-  micValFilteredPrev = micValFiltered;
   
   micVal = abs(analogRead(micPin)-512);
   micValFiltered = myFilter.step(micVal);
 
   Serial.println(micValFiltered);
+
+  delay(sampling_period_ms);
 }
 
